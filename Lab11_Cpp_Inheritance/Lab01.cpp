bf9b2cefd20c31cc9226b22965b1f2ed0955cf9d
OwlVi <110912364+OwlVi@users.noreply.github.com>
2023-03-08 18:37:08 +0700

    add Lab11 Inheritance

---
+++ Lab11_Cpp_Inheritance/Lab01.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MusicPlaylist
+{
+private:
+    string track[20];
+public:
+    int i = 0;
+    int j = 0;
+    MusicPlaylist(){
+        
+    }
+
+    void add(string Music){
+        if (track[i] == "\0"){
+            track[i] = Music;
+            i++;
+        }else{
+            track[i] = Music;
+            i++;
+        }
+    }
+    
+    void setCurrentTrack(int number){
+        j = number;
+    }
+
+    string play(){
+        return track[i];
+    }
+
+};
+
+int main()
+{
+    MusicPlaylist mp;
+    mp.add("Shape of You");
+    mp.add("That's What I Like");
+    mp.add("Humble");
+    mp.setCurrentTrack(1);
+    cout << mp.play() << endl; // แสดงเพลงลำดับที่ 1 คือ Shape of You
+}
