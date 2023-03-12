7301f25f26b5635b1886c47518fd54d280192160
OwlVi <110912364+OwlVi@users.noreply.github.com>
2023-03-13 02:59:24 +0700

    Lab03 [PPP---P-PP]

--- Lab11_Cpp_Inheritance/Lab03.cpp
+++ Lab11_Cpp_Inheritance/Lab03.cpp
@@ -21,16 +21,62 @@ public:
     void sleep(int);
 };
 
-Animal::Animal(int happiness,int energy,int fullness):happiness(happiness),energy(energy),fullness(fullness){}
+Animal::Animal(int happiness,int energy,int fullness):happiness(happiness),energy(energy),fullness(fullness){
+    limitStat();
+}
 
 int Animal::getHappiness(){return happiness;}
 int Animal::getEnergy(){return energy;}
 int Animal::getFullness(){return fullness;}
 
-void Animal::eat(int eat){}
-void Animal::play(int play){}
-void Animal::sleep(int sleep){}
+void Animal::eat(int eat){
+
+    this->fullness  += eat;
+    if (fullness > MAX_FULLNESS){
+        this->happiness -= 10;
+    }
+    limitStat();
+}
+void Animal::play(int play){
+
+    this->energy -= play*10;
+    this->fullness -= play*20;
+    this->happiness += play*5;
+    limitStat();
+}
+
+void Animal::sleep(int sleep){
+    this->energy += sleep*10;
+    this->fullness -= sleep*10;
+    limitStat();
+}
+
+void Animal::limitStat(){
+    if (happiness < 0){
+        happiness = 0;
+    }else if (happiness > MAX_HAPPINESS){
+        happiness = MAX_HAPPINESS;
+    }
+    if (energy < 0){
+        energy = 0;
+    }else if(energy > MAX_ENERGY){
+        energy = MAX_ENERGY;
+    }
+    if (fullness < 0){
+        fullness = 0;
+    }else if(fullness > MAX_FULLNESS){
+        fullness = MAX_FULLNESS;
+    }
+}
 
 
+int main(){
+    Animal pet(50,50,50);
+
+
+
+
+
+}
 
 
