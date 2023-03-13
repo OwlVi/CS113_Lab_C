1c186028fadbc5906e4353e376ab5983eed9cc3b
OwlVi <110912364+OwlVi@users.noreply.github.com>
2023-03-14 04:46:21 +0700

    add Lab03 | Lab11cpp |[P-PPPPPPPP]

--- Lab11_Cpp_Inheritance/Lab03.cpp
+++ Lab11_Cpp_Inheritance/Lab03.cpp
@@ -21,58 +21,61 @@ public:
     void sleep(int);
 };
 
-Animal::Animal(int happiness,int energy,int fullness):happiness(happiness),energy(energy),fullness(fullness){
-    limitStat();
-}
+Animal::Animal(int happiness,int energy,int fullness):happiness(happiness),energy(energy),fullness(fullness){limitStat();}
 
 int Animal::getHappiness(){return happiness;}
 int Animal::getEnergy(){return energy;}
 int Animal::getFullness(){return fullness;}
 
-void Animal::eat(int eat){
-
-    this->fullness  += eat;
-    if (fullness > MAX_FULLNESS){
-        this->happiness -= 10;
+void Animal::eat(int food){
+    for (int i = 1 ; i <= food ; i++){
+        this->fullness  += 1;
+        if (fullness > MAX_FULLNESS){
+            this->happiness -= 10;
+            return limitStat();
+        }
     }
-    limitStat();
-}
-void Animal::play(int play){
-
-    this->energy -= play*10;
-    this->fullness -= play*20;
-    this->happiness += play*5;
-    limitStat();
 }
 
-void Animal::sleep(int sleep){
-    this->energy += sleep*10;
-    this->fullness -= sleep*10;
+void Animal::play(int hour){
+    for (int i = 1; i <= hour ;i++){
+        if (fullness <= 0 || energy <= 0)return limitStat();
+        this->energy -= 10;
+        this->fullness -= 20;
+        this->happiness += 5;
+    }
+}
+
+void Animal::sleep(int hour){
+    for (int i = 1 ; i <= hour ; i++){
+        if (energy > MAX_ENERGY) return limitStat();
+        this->energy += 10;
+        this->fullness -= 10;
+    }
     limitStat();
 }
 
 void Animal::limitStat(){
-    if (happiness < 0){
-        happiness = 0;
-    }else if (happiness > MAX_HAPPINESS){
-        happiness = MAX_HAPPINESS;
-    }
-    if (energy < 0){
-        energy = 0;
-    }else if(energy > MAX_ENERGY){
-        energy = MAX_ENERGY;
-    }
-    if (fullness < 0){
-        fullness = 0;
-    }else if(fullness > MAX_FULLNESS){
-        fullness = MAX_FULLNESS;
-    }
+    if (happiness < 0){happiness = 0;}
+    else if (happiness > MAX_HAPPINESS){happiness = MAX_HAPPINESS;}
+    if (energy < 0){energy = 0;}
+    else if(energy > MAX_ENERGY){energy = MAX_ENERGY;}
+    if (fullness < 0){fullness = 0;}
+    else if(fullness >= MAX_FULLNESS){fullness = MAX_FULLNESS;}
 }
 
 
 int main(){
-    Animal pet(50,50,50);
+    Animal pet(100,100,100);
 
+    pet.play(10);
+    cout << "Happy = " << pet.getHappiness()<< " Full = "
+         << pet.getFullness() << " Energy = "
+         << pet.getEnergy() << endl;
+    pet.eat(101);
+    cout << "Happy = " << pet.getHappiness()<< " Full = "
+         << pet.getFullness() << " Energy = "
+         << pet.getEnergy() << endl;
 
 
 
